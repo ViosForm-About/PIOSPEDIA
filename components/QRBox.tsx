9092ad069e8a8d7850141a3705562decc66ef2f8
Vios About <tyzcelk2@gmail.com>
2025-12-30 12:27:57 +0700

    Update QRBox.tsx

--- components/QRBox.tsx
+++ components/QRBox.tsx
@@ -1,9 +1,18 @@
-export default function QRBox({ qr, total, id }: any) {
+'use client'
+import Link from 'next/link'
+
+export default function QRBox({ data }: any) {
   return (
-    <div>
-      <img src={qr} width={200} />
-      <p>Total: Rp{total}</p>
-      <a href={`/status/${id}`}>Cek Status</a>
+    <div className="mt-6 text-center space-y-2">
+      <img src={data.qrImage} className="mx-auto w-64" />
+      <p>Nominal: Rp {data.nominal}</p>
+      <p>Admin: Rp {data.adminFee}</p>
+      <Link
+        href={`/status/${data.transactionId}`}
+        className="inline-block mt-4 px-4 py-2 bg-blue-500 rounded"
+      >
+        Cek Status
+      </Link>
     </div>
   )
 }
